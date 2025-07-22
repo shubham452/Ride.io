13d0147960a40dd4e2df9315763bc39256368505
shubham452 <shubham.sk70@gmail.com>
2025-07-22 21:36:40 +0530

    built login logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './App.css'
-
+import Login from './components/Login'
+import Register from './components/Register'
+import Home from './components/Home';
 function App() {
   const [count, setCount] = useState(0)
 
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {Link} from 'react-router-dom'
 
 const Login = () => {
+    const [input, setInput] = useState({
+        email:"",
+        password:"",
+    });
+
+    const handleInput=(e)=>{
+        setInput({...input,[e.target.name]:e.target.value});
+    }
+    const handleSubmit=async(e)=>{
+        e.preventDefault();
+        console.log(input);
+    }
     return (
         <div>
-            Login
+            <h2>Sign in</h2>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor='email'>
+                    Email:
+                </label>
+                <input
+                type='email'
+                name='email'
+                value={input.email}
+                placeholder="email"
+                onChange={(e)=>{
+                    handleInput(e);
+                }}/>
+
+                <label htmlFor='password'>
+                    Password:
+                </label>
+                <input
+                type='password'
+                name='password'
+                value={input.password}
+                placeholder="Password"
+                onChange={(e)=>{
+                    handleInput(e);
+                }}/>
+                <button type='submit'>Log in</button>
+                <p>Don't have an account?<Link to='/register'>Sign up</Link></p>  
+            </form>
         </div>
     )
 }
